ec04aca5f21100fcc9b7e2ace6ad0420974c747c
Sudeep Aryan <sudeeparyan.g@solitontech.com>
2023-02-09 14:41:08 +0530

    feat: Java Script: add interactiveness to city container

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ fetch("data.json")
     weather_data = result;
     console.log(weather_data);
     setWeather();
+    change();
   });
 
 function setWeather() {
   var city = Object.keys(weather_data);
   var option = ``;
   for (let i = 0; i < city.length; i++) {
-    option += `<option>${city[i]}</option>`;
+    option += `<option>${weather_data[city[i]].cityName}</option>`;
   }
   document.querySelector("#data_dropdown").innerHTML = option;
 }
+function firstcity() {
+  var city = Object.keys(weather_data);
+  document.getElementById("inputdata").innerHTML = city[0];
+  console.log(document.getElementById("inputdata").value);
+  change();
+}
+
 //temperature
 let far;
 function changeToFarenheit(val) {
@@ -22,9 +30,46 @@ function changeToFarenheit(val) {
   console.log("this is inside function", farenheit);
   return farenheit;
 }
-function change() {
+let selectedCity;
+
+function callChange() {
+  selectedCity = document.querySelector("#inputdata").value.toLowerCase();
+  city = Object.keys(weather_data);
+  let currentCity = selectedCity;
+  let flag = 0;
+  for (let i = 0; i < city.length; i++) {
+    if (currentCity == city[i]) {
+      console.log("main");
+      change(currentCity);
+      flag = 1;
+    }
+  }
+  if (flag == 0) {
+    Null();
+  }
+}
+function Null() {
+  document.querySelector("#top-tempc").innerText = "-";
+  document.querySelector("#top-far").innerText = "-";
+  document.querySelector("#top-humidity").innerText = "-";
+  document.querySelector("#top-precipitation").innerText = "-";
+  document.querySelector("#top-date").innerText = "";
+  document.querySelector("#top-time").innerText = "Enter a valid City";
+  document.querySelector("#data_dropdown").style.borderColor = "red";
+  document.querySelector("#top-time").style.color = "";
+  document.querySelector("#top-img").src = "";
+  for (let i = 0; i < 6; i++) {
+    document.querySelector(`#time-${i + 1}`).innerText = "-";
+    document.querySelector(`#icon-${i + 1}`).src = "";
+    document.querySelector(`#temperature-${i + 1}`).innerText = "-";
+  }
+}
+
+function change(currentCity) {
+  var dropdown = currentCity;
+  console.log(dropdown);
   var city = Object.keys(weather_data);
-  let montharr = [
+  let monthArr = [
     "Jan",
     "Feb",
     "Mar",
@@ -38,7 +83,6 @@ function change() {
     "Nov",
     "Dec",
   ];
-  var dropdown = document.getElementById("inputdata").value;
   //Image
   document.getElementById(
     "top-img"
@@ -53,48 +97,15 @@ function change() {
   document.getElementById("top-precipitation").innerHTML =
     weather_data[dropdown].precipitation;
   //temperature F
-  let cel = weather_data[dropdown].temperature.slice(0, -2);
+  let cel = parseInt(weather_data[dropdown].temperature);
   far = changeToFarenheit(cel).toFixed(0) + "F";
   document.getElementById("top-far").innerHTML = far;
-  //Date and time
+  //Date
   let dateArr;
   dateArr = weather_data[dropdown].dateAndTime.split(",");
   console.log(dateArr[1].slice(0, -2));
-  document.getElementById("top-time-1").innerHTML = dateArr[1].slice(0, -2);
+  document.getElementById("top-time").innerHTML = dateArr[1].slice(0, -2);
   document.getElementById("top-date").innerHTML = dateArr[0];
-
-  // Date Spliting Method
-  let dateSplit = dateArr[0];
-  console.log(dateSplit);
-  dateArr = dateSplit.split("/");
-  let dateInWords =
-    dateArr[1] + "-" + montharr[parseInt(dateArr[0]) - 1] + "-" + dateArr[2];
-  document.getElementById("top-date").innerHTML = dateInWords;
-
-  // Time
-  let timerId;
-  function timedate(timeZone, dateAndTime) {
-    if (timerId) {
-      clearInterval(timerId);
-    }
-    timerId = setInterval(() => {
-      var date = new Date()
-        .toLocaleString("en-US", {
-          timeZone: timeZone,
-          timeStyle: "medium",
-          hourCycle: "h24",
-        })
-        .split(":");
-      document.getElementById("top-time-1").innerHTML = date[0] + ":" + date[1];
-      document.getElementById("time-color").innerHTML = date[2];
-    }, 100);
-  }
-  for (var i = 0; i < city.length; i++) {
-    var time = weather_data[dropdown].timeZone;
-    var dateAndTime = weather_data[dropdown].dateAndTime;
-    console.log(weather_data[dropdown].temperature, time, dateAndTime);
-    timedate(time, dateAndTime);
-  }
 
   // Temperature Changing Left
   let sixtemp = [
@@ -129,6 +140,43 @@ function change() {
     } else if (sixtemp[i] > 29) {
       document.querySelector(`#icon-${i + 1}`).src =
         "HTML & CSS/Weather Icons/sunnyIcon.svg";
+    }
+  }
+
+  // Time And Date
+  let timerId;
+  let time;
+
+  time = new Date().toLocaleString("en-US", {
+    timeZone: weather_data[selectedCity].timeZone,
+    timeStyle: "medium",
+    hourCycle: "h12",
+  });
+
+  document.getElementById("top-time").innerHTML = time;
+  console.log(time);
+
+  //Hours changing with wrt to time.
+  let hour = parseInt(time.split(":")[0]);
+  let noon = time.split(" ")[1];
+
+  for (let i = 0; i < 6; i++) {
+    if (hour > 12) {
+      hour = hour - 12;
+    }
+    if (i == 0) {
+      document.querySelector(`#time-${i + 1}`).innerHTML = "NOW";
+    } else {
+      document.querySelector(`#time-${i + 1}`).innerHTML = hour + " " + noon;
+    }
+    if (hour == 11 && noon == "PM") {
+      noon = "AM";
+      hour = 12;
+    } else if (hour == 11 && noon == "AM") {
+      hour = 12;
+      noon = "PM";
+    } else {
+      hour++;
     }
   }
 }
