70924aacaac3fe27b8f1e91ae9dca315b248464f
Sudeep Aryan <sudeeparyan.g@solitontech.com>
2023-02-14 12:02:13 +0530

    stash apply 2

--- export.js
+++ export.js
@@ -1,5 +1,11 @@
+/**Changes to celiuses to farenheit
+ * @param {*Number} val  containes the celiuses value 
+ * @returns farenheit value.
+ */
 export default function changeToFarenheit(val) {
     let farenheit = val * 1.8 + 32;
     console.log("this is inside function", farenheit);
     return farenheit;
 }
+
+
