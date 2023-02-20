6a5fe0cae48aaee3e70e2da5cfaed03d714c88be
Sudeep Aryan <sudeeparyan.g@solitontech.com>
2023-02-20 15:49:26 +0530

    Refactor: Java Script changed function names and kept common date function

--- export.js
+++ export.js
@@ -6,8 +6,3 @@ export default function changeToFarenheit(val) {
     let farenheit = val * 1.8 + 32;
     return farenheit;
 }
-let setCityTimeZones = function (city) {
-    return city.timeZone.split("/")[0];
-  };
-
-  export{setCityTimeZones}
