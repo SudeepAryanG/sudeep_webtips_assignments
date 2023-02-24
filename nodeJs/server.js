63777933429fce221970035c8865ab562e0ad9d4
Sudeep Aryan <sudeeparyan.g@solitontech.com>
2023-02-24 10:35:55 +0530

    Refactor: change path of images

--- nodeJs/server.js
+++ nodeJs/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const path = require('path')
 app.use(express.static(path.join(__dirname,"src" )))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
